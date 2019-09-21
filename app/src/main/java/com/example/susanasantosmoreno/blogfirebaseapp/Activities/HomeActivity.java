56380a54efcfb138920e6cf18758a4cf4f77abfa
Susana Santos Moreno <susiylola@gmail.com>
2019-09-21 15:03:04 +0200

    Navigation Drawer created with user credentials. App theme changed.

--- app/src/main/java/com/example/susanasantosmoreno/blogfirebaseapp/Activities/HomeActivity.java
+++ app/src/main/java/com/example/susanasantosmoreno/blogfirebaseapp/Activities/HomeActivity.java
@@ -1,15 +1,132 @@
 package com.example.susanasantosmoreno.blogfirebaseapp.Activities;
 
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import com.example.susanasantosmoreno.blogfirebaseapp.R;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class HomeActivity extends AppCompatActivity {
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.ImageFormat;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.susanasantosmoreno.blogfirebaseapp.R;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private NavigationView navigationView;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+
+    private TextView navUserName;
+    private TextView navUserMail;
+    private ImageView navUserImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        this.setTitle("Home");
+
+        //init views and firebase.
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        updateNavHeader();
     }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch ( menuItem.getItemId()){
+            case R.id.nav_item_one:
+                showMessage("Item one");
+                break;
+            case R.id.nav_item_two:
+                showMessage("Item two");
+                break;
+            default:
+                return true;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    public void updateNavHeader(){
+        View headerView = navigationView.getHeaderView(0);
+        navUserName = headerView.findViewById(R.id.navUserName);
+        navUserMail = headerView.findViewById(R.id.navUserMail);
+        navUserImg = headerView.findViewById(R.id.navUserImage);
+
+        navUserMail.setText(currentUser.getEmail());
+        navUserName.setText(currentUser.getDisplayName());
+
+        Glide.with(this).load(currentUser.getPhotoUrl()).into(navUserImg);
+    }
+
+
 }
